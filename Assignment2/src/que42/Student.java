494af00af02bd4a1018ef31436a07ca4b132d143
srushtii13 <127539974+srushtii13@users.noreply.github.com>
2024-01-18 22:09:03 +0530

    Add files via upload

---
+++ Assignment2/src/que42/Student.java
@@ -0,0 +1,17 @@
+package que42;
+
+public class Student {
+	
+	private int rollNo;
+	private float percentage;
+	
+	public Student() {
+		
+	}
+	public Student(int rollNo , float percentage) {
+		this.rollNo = rollNo;
+		this.percentage = percentage;
+	}
+	
+
+}
